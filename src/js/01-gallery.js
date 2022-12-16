0083c53abe1a6a124ef4d7cdf94d9aab0e0d33fd
VitaBabenko <babenkovita1988@gmail.com>
2022-12-16 20:23:20 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+hello = 'hello world';
+console.log(hello);
 console.log(galleryItems);
