d3a6940eae391df426d7b6ce0fc79a0ff0237654
VitaBabenko <babenkovita1988@gmail.com>
2022-12-19 23:29:16 +0200

    changed 2js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+// console.log(Player);
 
+const iframe = document.querySelector('#vimeo-player');
+// console.log(iframe);
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const onTimeupdate = function (seconds) {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+    // console.log("play");
+};
+    
+player.on('timeupdate', onTimeupdate);
+
+player.setCurrentTime().then(function (seconds) {
+    seconds = localStorage.getItem(LOCALSTORAGE_KEY);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // error.name < 0 || error.name > duration;
+            break;
+        
+        default:
+            break;
+    }
+});
