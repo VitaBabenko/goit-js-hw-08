99de39af078ab954c435a8c23089d10c36c69732
VitaBabenko <babenkovita1988@gmail.com>
2022-12-19 15:58:06 +0200

    changed 1js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,21 @@
 import SimpleLightbox from "simplelightbox";
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// hello = 'hello world';
-// console.log(hello);
 // console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+// console.log(gallery);
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`}).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+// console.log(gallery);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+console.log(lightbox);
